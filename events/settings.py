cd1b9dd1b0490267d0e5e1f88a0d835b31233d48
vikas-tuteja <htcvikas>
2017-06-17 12:50:19 +0530

    set up using openshift

--- events/settings.py
+++ events/settings.py
@@ -82,22 +82,21 @@ WSGI_APPLICATION = 'wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-from . import database
+#from . import database
+#DATABASES = {
+#    'default': database.config()
+#}
 DATABASES = {
-    'default': database.config()
-}
-"""DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'wse',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'root',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'worksrqn_wse',                      # Or path to database file if using sqlite3.
+        'USER': 'worksrqn_root',                      # Not used with sqlite3.
+        'PASSWORD': 'worksrqn_root',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     },
 
-}"""
+}
 
 
 
