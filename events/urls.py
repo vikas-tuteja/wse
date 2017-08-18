4997d610de5a3da29ff5f8f7963d7a51fc657f2b
vikas-tuteja <htcvikas>
2017-08-19 00:50:15 +0530

    footer links

--- events/urls.py
+++ events/urls.py
@@ -5,8 +5,8 @@ from views import EventListing, RequirementListing, EventDetail, ApplyForRequire
 urlpatterns = [
     # event listing urls
     url(r'^events/$', EventListing.as_view(), name="event_listing"),
-    url(r'^events-in-(?P<city_slug>[-\w]+)-city/$', EventListing.as_view(), name="event_listing"),
-    url(r'^events-in-(?P<area_slug>[-\w]+)/$', EventListing.as_view(), name="event_listing"),
+    url(r'^events-in-(?P<city_slug>[-\w]+)-city/$', EventListing.as_view(), name="event_listing_city"),
+    url(r'^events-in-(?P<area_slug>[-\w]+)/$', EventListing.as_view(), name="event_listing_area"),
     
     
     url(r'^post-events/$', PostEvents.as_view(), name="post_events"),
