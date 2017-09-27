f1a9c692ce4d9866e3244d023d82b0680c3e11ba
vikas-tuteja <htcvikas>
2017-09-27 21:44:24 +0530

    email

--- utility/email_config.py
+++ utility/email_config.py
@@ -11,14 +11,41 @@ email_data = {
                     Password - %(password)s<br>\
                     <br>\
                     WorkSmart Events is a platform that bridges the gap between event organizers and resources required for the events.<br>\
-                    As an esteemed client you have an opportunity to post a requirement and we will provide you with the screened candidates to make the event a success (<a href="http://www.worksmartevents.com/termsandconditions">T&C Apply)<br>\
+                    As an esteemed client you have an opportunity to post a requirement and we will provide you with the screened candidates to make the event a success (<a href="http://www.worksmartevents.com/termsandconditions">T&C Apply</a>)<br>\
                     Someone from our team would get back to you in the next 24 hours to understand your requirements better before posting them on the website.<br>\
                     You may start posting your requirements at - http://www.worksmartevents.com/post-events/<br>\
                     You may update your profile from - http://www.worksmartevents.com/my-profile/overview/<br>\
-                    In case of any queries please get in touch with us at +91-702-111-5997 (8AM - 9PM) or email at worksmartevent@gmail.com<br>\
+                    In case of any queries please get in touch with us at +91-702-111-5997 (8AM - 9PM) or email us at worksmartevent@gmail.com<br>\
                     <br>\
                     Regards, <br>\
                     WorkSmart Events Team<br>\
+                    </div>',
+            'subject': 'Welcome to Worksmart Events',
+            'bcc_address': '',
+            'plain_text': ''
+        }
+    }
+
+    'candidate': {
+        'welcome': {
+            'html': '<div style="font-family:Roboto;">\
+                    Dear %(username)s,<br>\
+                    <br>\
+                    Thank You for registering with WorkSmart Events. We extend you a warm welcome from the WSE community <br>\
+                    <br>\
+                    Your login credentials are as follows - <br>\
+                    Username - %(username)s<br>\
+                    Password - %(password)s<br>\
+                    <br>\
+                    WorkSmart Events is a platform that bridges the gap between event organizers and resources required for the events.<br>\
+                    As an esteemed candidate you have an opportunity to engage and work with big brands for short/long durations and get handsomely paid for your contribution. (<a href="http://www.worksmartevents.com/termsandconditions">T&C Apply</a>)
+                    Someone from our team would get back to you in the next 24 hours to verify your details <br>\
+                    You may start applying for events by visiting - http://www.worksmartevents.com/events/<br>\
+                    You may update your profile from - http://www.worksmartevents.com/my-profile/overview/<br>\
+                    In case of any queries please get in touch with us +91-702-111-5997 (8AM - 9PM) or email us at worksmartevent@gmail.com<br>\
+                    <br>\
+                    Regards, <br>\
+                    WorkSmart Events Team<br>\,
                     </div>',
             'subject': 'Welcome to Worksmart Events',
             'bcc_address': '',
