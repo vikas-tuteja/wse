e146ab7d31a5d31a223657d6d31c084adb142a15
vikas-tuteja <htcvikas>
2017-10-03 13:24:33 +0530

    sitemap

--- wse/urls.py
+++ wse/urls.py
@@ -29,11 +29,12 @@ from master.sitemap import sitemap, sitemap_redirect
 # url patterns here
 urlpatterns = [
     url(r'^google7016c5c21c248bea.html', lambda r: HttpResponse("google-site-verification: google7016c5c21c248bea.html", content_type="text/plain")),
+    url(r'^sitemap\.xml$', TemplateView.as_view(template_name='sitemap.xml', content_type='text/xml')),
     url(r'^$', Home.as_view(), name="home"), 
     url(r'^admin/', admin.site.urls),
     url(r'^static/(?P<path>.*)$', django.views.static.serve,  {'document_root': settings.STATIC_ROOT }),
     url(r'^uploads/profile/(?P<path>.*)$', django.views.static.serve,  {'document_root': '%s/uploads/profile' % settings.BASE_DIR }),
-    url(r'^sitemap.xml$', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
+    #url(r'^sitemap.xml$', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
     url(r'^sitemap-(?P<section>.+)\.xml$', dsitemap, {'sitemaps': sitemaps}, name="section_sitemap"),
     url(r'^sitemap_(?P<section>.+)$', sitemap_redirect, name="redirect_section"),
     url(r'^master/', include('master.urls')),
