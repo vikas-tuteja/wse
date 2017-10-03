e6a96aaacb2afa5cec2395cbac194c0b0941e1b7
vikas-tuteja <htcvikas>
2017-10-03 13:25:44 +0530

    sitemap

--- miscellanous/urls.py
+++ miscellanous/urls.py
@@ -4,8 +4,8 @@ from .views import *
 urlpatterns = [
     url(r'^faqs/$', Faqs.as_view(), name="faqs"),
     url(r'^clients/$', Clients.as_view(), name="clients"),
-    url(r'^contact_us/$', ContactUs.as_view(), name="contact_us"),
-    url(r'^about_us/$', AboutUs.as_view(), name="about_us"),
+    url(r'^contact-us/$', ContactUs.as_view(), name="contact_us"),
+    url(r'^about-us/$', AboutUs.as_view(), name="about_us"),
 
     url(r'^termsandconditions/$', TNC.as_view(), name="tnc"),
     url(r'^privacypolicy/$', PrivacyPolicy.as_view(), name="privacypolicy"),
